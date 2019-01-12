c0da41bb9681bb0c444f50d769b6c285b5d7fb20
nyzo-voting-repo <46629181+nyzo-voting-repo@users.noreply.github.com>
2019-01-12 17:08:13 +0100

    Create tst.sh

---
+++ tst.sh
@@ -0,0 +1,25 @@
+# change path if necessary, amazon servers path might be something like this: /nyzoVerifier/build/libs/.....
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1
