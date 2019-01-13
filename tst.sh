8057247241cb4694e31a53f3022b7e6b15eedfdb
nyzo-voting-repo <46629181+nyzo-voting-repo@users.noreply.github.com>
2019-01-13 20:24:26 +0100

    Update tst.sh

--- tst.sh
+++ tst.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # change path if necessary, amazon servers path might be something like this: /nyzoVerifier/build/libs/.....
 sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
 sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 &
