d6462cd014ddb2ea9f9c5797a6090d2196ce347a
cryptic-monk <monk@whitespace.financial>
2019-01-22 18:39:49 +0100

    Don’t kill processes after system reboot

--- tst.sh
+++ tst.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 # change path if necessary, amazon servers path might be something like this: /nyzoVerifier/build/libs/.....
+
 ./kill.sh
 rm kill.sh
-echo "echo 'killing previous processes'" >> kill.sh
+echo "#!/bin/sh\necho 'killing previous processes'" >> kill.sh
+
+sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 & echo "a=$!\nif [ \"\$\$\" -lt \"\$a\" ];then\necho 'system restart, exiting'\nexit 0\nfi\nsudo kill $!" >> kill.sh
 sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 & echo "sudo kill $!" >> kill.sh &
 sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 & echo "sudo kill $!" >> kill.sh &
 sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 & echo "sudo kill $!" >> kill.sh &
@@ -26,7 +29,7 @@ sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo
 sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 & echo "sudo kill $!" >> kill.sh &
 sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 & echo "sudo kill $!" >> kill.sh &
 sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 & echo "sudo kill $!" >> kill.sh &
-sudo java -jar /home/ubuntu/nyzoVerifier/build/libs/nyzoVerifier-1.0.jar co.nyzo.verifier.scripts.NewVerifierVoteOverrideRequestScript  YOUR-INCYCLE-VERIFIER-PRIVATE-KEY $1 & echo "sudo kill $!" >> kill.sh &
+
 chmod +x kill.sh
 wait
 
